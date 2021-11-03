d155015e143cfa9260a7d08795996e2c98ddbe97
Jesús Caro <jfcarocota@gmail.com>
2021-11-03 14:57:04 -0700

    collider changes
    Cambios en el collider para las ramas y el escalado en el árbol.

--- Assets/Script/Einho.cs
+++ Assets/Script/Einho.cs
@@ -24,6 +24,10 @@ public class Einho : MonoBehaviour
     [SerializeField]
     LayerMask detectionLayer;
 
+    bool isAttacking = false;
+    [SerializeField]
+    AnimationClip attackClip;
+
     void Awake()
     {
         spr = GetComponent<SpriteRenderer>();
@@ -44,12 +48,24 @@ public class Einho : MonoBehaviour
         {
             Jump();
         }
+        if(Attack && !isAttacking)
+        {
+            StartCoroutine(DoAttack());
+        }
     }
 
     void Jump()
     {
         rb2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         anim.SetTrigger("jump");
+    }
+
+    IEnumerator DoAttack()
+    {
+        isAttacking = true;
+        anim.SetTrigger("attack");
+        yield return new WaitForSeconds(attackClip.length);
+        isAttacking = false;
     }
 
     void LateUpdate()
@@ -68,5 +84,6 @@ public class Einho : MonoBehaviour
     bool FlipSprite => Axis.x > 0f ? false : Axis.x < 0f ? true : spr.flipX;
     bool IsJumping => Input.GetButtonDown("Jump");
     bool Grounding => Physics2D.Raycast(transform.position, Vector2.down, rayDistance, detectionLayer);
+    bool Attack => Input.GetButtonDown("Fire1");
 
 }
