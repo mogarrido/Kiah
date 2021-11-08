89c9c3d7a7bcc35d67b94f6cdead8ca901dcbd7b
samgonzale <samgonzale@cedim.edu.mx>
2021-11-08 12:32:10 -0600

    boss first try

---
+++ Assets/Script/BOSS_Naelie.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class BOSS_Naelie : MonoBehaviour
+{
+    public Slider healthBar;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
