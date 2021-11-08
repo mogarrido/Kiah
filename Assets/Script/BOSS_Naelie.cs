05074df0d9afa69fa75ea986618cb1a303316797
samgonzale <samgonzale@cedim.edu.mx>
2021-11-08 16:26:34 -0600

    hjnj

--- Assets/Script/BOSS_Naelie.cs
+++ Assets/Script/BOSS_Naelie.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+<<<<<<< Updated upstream
 public class BOSS_Naelie : MonoBehaviour
 {
     public Slider healthBar;
 
 
+=======
+
+public class BOSS_Naelie : MonoBehaviour
+{
+>>>>>>> Stashed changes
     // Start is called before the first frame update
     void Start()
     {
