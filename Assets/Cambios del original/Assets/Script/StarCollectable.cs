4556a7695d02bf31f10dac320f431f6797bcbe96
LudwikaG <wikkina10ga@gmail.com>
2021-11-08 12:46:23 -0600

    cambios

---
+++ Assets/Cambios del original/Assets/Script/StarCollectable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarCollectable : Collectable
+{
+    Animator animator;
+    public string AnimationName  = "POLVO";
+    public float DestroyAfter = 1.0f;
+
+    void Start(){
+        animator = GetComponent<Animator>();
+    }
+    public override void OnTriggerEnter2D(Collider2D other)
+    {
+        base.OnTriggerEnter2D(other);
+        if (other.tag == "Player")
+        {
+            
+            animator.Play(AnimationName);
+            Destroy(this.gameObject, DestroyAfter);
+        }
+    }
+}
