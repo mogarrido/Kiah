920e32265fd4110532267fe91daa889d7a5bcc67
M3405 <erickmendro@hotmail.com>
2021-11-16 18:04:31 -0600

    Creditos con programación
    Ya esta agregado la escena de creditos con programación y agregue un boton en la escena de menu principal.

---
+++ Assets/Script/Creditos.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Creditos : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("WaitToEnd",65); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            SceneManager.LoadScene ("Pantalla_inicio");
+        }
+    }
+
+
+    public void WaitToEnd()
+
+    {
+        SceneManager.LoadScene("Pantalla_inicio");
+    
+    }
+}
---
+++ Assets/Script/Pantalla_inicio.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pantalla_inicio : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Creditos ()
+
+    {
+        SceneManager.LoadScene("Creditos");
+    
+    }
+
+
+}
