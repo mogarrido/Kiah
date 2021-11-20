227bbbb3270eae07e6a276567f1883fe451c4778
LudwikaG <wikkina10ga@gmail.com>
2021-11-20 16:51:04 -0600

    avispa

--- Assets/Script/Avispa.cs
+++ Assets/Script/Avispa.cs
@@ -2,53 +2,158 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Avispa : MonoBehaviour
+public class avispa : Enemy
 {
-    private GameObject player;
-    public float speed;
-    public bool chase = false; 
-    public Transform startingPoint; 
-   
+    //Attack area
+    [SerializeField, Range(0.1f, 20f)]
+    float areaRadius = 5f;
+    [SerializeField]
+    Color areaColor = Color.red;
+    [SerializeField]
+    LayerMask areaDetectionLayer;
+    IEnumerator actualCoroutine;
+
+   [SerializeField]
+    Collider2D headcolliderLeft;
+    [SerializeField]
+    Collider2D headcolliderRight;
+
+     //Raycast things
+    [SerializeField, Range(0.1f, 20f)]
+    float rayDistance = 5f;
+    [SerializeField]
+    Color rayColor = Color.red;
+    [SerializeField]
+    LayerMask detectionLayer;
     
     
-     
+    [SerializeField]
+    float sleepTime = 2f;
+    [SerializeField]
+    float patrolTime = 5f;
+    float patrolTimer = 0f;
+    float sleepTimer = 0f;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
+        actualCoroutine = IdleCoroutine(sleepTime, "patrol");
+        StartCoroutine(IdleCoroutine(sleepTime, "patrol"));
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(player == null)
-           return; 
-        if (chase==true)
-           return; 
+        if (Die)
+        {
+            if(!diying)
+            {
+                diying = true;
+                anim.SetTrigger("die");
+                DeleteFromScene();
+            }
+            return;
+        }
+        if(CanAttack && !isAttacking)
+        {
+            lastFlip = spr.flipX;
+            StartCoroutine(AttackCoroutine("attack", attackClip, actualCoroutine));
+            StopCoroutine(actualCoroutine);
+        }
+        if(isAttacking && CanAttack)
+        {
+            spr.flipX = RightRay ? false : LeftRay ? true : spr.flipX;
+        } 
+    }
+    bool CanAttack => Physics2D.OverlapCircle(transform.position, areaRadius, areaDetectionLayer);
 
-           
-        else 
-           //go to starting point
-           ReturnStartPosition(); 
-           Flip (); 
-
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = areaColor;
+        Gizmos.DrawWireSphere(transform.position, areaRadius);
+        Gizmos.color = rayColor;
+        Gizmos.DrawRay(transform.position, Vector2.right * rayDistance);
+        Gizmos.DrawRay(transform.position, Vector2.left * rayDistance);
     }
 
-    private void Chase ()
+     public override IEnumerator IdleCoroutine(float duration, string stateName)
     {
-        transform.position=Vector2.MoveTowards(transform.position,player.transform.position,speed * Time.deltaTime); 
+        anim.SetBool(stateName, false);
+        while(true)
+        {
+            sleepTimer += Time.deltaTime;
+            if(sleepTimer >= duration)
+            {
+                sleepTimer = 0f;
+                actualCoroutine = MovementCoroutine(patrolTime, "patrol");
+                StartCoroutine(actualCoroutine);
+                break;
+            }
+            yield return null;
+        }
     }
-    
-    private void ReturnStartPosition()
+
+    public override IEnumerator MovementCoroutine(float duration, string stateName)
     {
-        transform.position = Vector2.MoveTowards(transform.position, startingPoint.position, speed * Time.deltaTime); 
+        anim.SetBool(stateName, true);
+        spr.flipX = !spr.flipX;
+        direction = new Vector2(-direction.x, direction.y);
+        while(true)
+        {
+            if (Die)
+            {
+                break;
+            }
+            patrolTimer += Time.deltaTime;
+            if(patrolTimer >= duration)
+            {
+                patrolTimer = 0f;
+                actualCoroutine = IdleCoroutine(sleepTime, "patrol");
+                StartCoroutine(actualCoroutine);
+                break;
+            }
+            transform.Translate(direction * moveSpeed * Time.deltaTime);
+            yield return null;
+        }
     }
-    private void Flip()
+
+    void ActivateCollider()
     {
-        if(transform.position.x>player.transform.position.x)
-        transform.rotation = Quaternion.Euler(0,0,0);
+        if(spr.flipX)
+        {
+            headcolliderLeft.enabled = true;
+            headcolliderRight.enabled = false;
+        }
         else
-        transform.rotation = Quaternion.Euler(0,180,0);
+        {
+            headcolliderLeft.enabled = false;
+            headcolliderRight.enabled = true;
+        }
     }
 
+    void DesableCollider()
+    {
+        headcolliderLeft.enabled = false;
+        headcolliderRight.enabled = false;
+    }
+
+    void MakeDamageToPlayer()
+    {
+        GameManager.instance.GetPlayer.RecivingDamage(damage);
+        GameManager.instance.GetHealthBar.SetValue(GameManager.instance.GetPlayer.GetHealth);
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(!isMakingDamage)
+        {
+            isMakingDamage = true;
+            MakeDamageToPlayer();
+        }
+    }
+
+    bool RightRay => Physics2D.Raycast(transform.position, Vector2.right, rayDistance, detectionLayer);
+    bool LeftRay => Physics2D.Raycast(transform.position, Vector2.left, rayDistance, detectionLayer);
+    bool Die => health == 0;
 }
